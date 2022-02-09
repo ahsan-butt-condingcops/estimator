5b6cb7a8b6abc7d7a264ea3c1a12076293625ecd
ahsan-butt-condingcops <ahsan.butt@codingcops.com>
2022-02-09 23:42:46 +0500

    wickedpdf

--- app/controllers/cost_estimates_controller.rb
+++ app/controllers/cost_estimates_controller.rb
@@ -509,6 +509,7 @@ class CostEstimatesController < ApplicationController
     respond_to do |format|
       format.pdf do
         render template: "cost_estimates/print_pdf.html.erb",
+               :pdf          => 'test',
                :save_to_file => Rails.root.join('public', "test.pdf"),
                :save_only    => true
       end
